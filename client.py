a6319981b759f53e3233d542fb982fc545d89ad3
Anmol Shukla <dev@sproutsai.com>
2025-01-31 22:06:35 +0530

    integrating llm

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 st.title('RAG Chatbot!')
 
@@ -13,6 +17,19 @@ prompt = st.chat_input('Pass your prompt here!')
 if prompt:
     st.chat_message('user').markdown(prompt)
     st.session_state.messages.append({'role': 'user', 'content': prompt})
-    response = 'Hello! I am your Assistant!'
+
+    groq_system_prompt = ChatPromptTemplate.from_template(
+        "You are very smart at everything, you always give the best, the most accurate and most precise answers. Answer the following Question: {user_prompt}. Start the answer directly. No small talk please."
+    )
+
+    model = 'llama3-8b-8192'
+    groq_chat = ChatGroq(
+        api_key=os.environ.get('GROQ_API_KEY'),  
+        model_name=model
+    )
+
+    chain = groq_system_prompt | groq_chat | StrOutputParser()
+    response = chain.invoke({"user_prompt": prompt}) 
+
     st.chat_message('assistant').markdown(response)
     st.session_state.messages.append({'role': 'assistant', 'content': response})
